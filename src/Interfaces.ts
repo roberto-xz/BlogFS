d719f491cc02c78ff95527cf417bc4de135ff986
roberto-xz <devv-robert@outlook.com>
2025-12-07 09:40:00 -0300

    feat(interfaces): adicionar interface meta_block_session
    Define os metadados de cada bloco.
    
    Campos:
    - label (15 bytes UTF-8): identificação do bloco
    - stats (1 byte): status (soft delete)
    - r_count (4 bytes): quantidade de registros
    - r_session_address (8 bytes): endereço inicial da sessão de registros

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -4,3 +4,10 @@ export interface meta_file_head_session {
     a_byte: Uint8Array; //versão do arquivo + flag
     b_byte: Uint8Array; //quantidade total de blocos"   
 }
+
+export interface meta_block_session {
+   label: string;     // (15 bytes UTF-8): identificação do bloco
+   stats: Uint8Array; // status (soft delete)
+   r_count: Uint32Array;             // quantidade de registros
+   r_session_address:BigUint64Array; // endereço inicial da sessão de registros"
+}
