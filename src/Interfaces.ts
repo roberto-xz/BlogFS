9566deaf551c042c1f190980055c188fff92e2c5
roberto-xz <devv-robert@outlook.com>
2025-12-07 09:10:00 -0300

    feat(interfaces): adicionar interface meta_file_head_session
    Define o cabeçalho principal do arquivo FS.
    
    Campos:
    - a_byte (1 byte): versão do arquivo + flag
    - b_byte (1 byte): quantidade total de blocos

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -1,1 +1,6 @@
 // [~] coded by roberto-xz
+
+export interface meta_file_head_session {
+    a_byte: Uint8Array; //versão do arquivo + flag
+    b_byte: Uint8Array; //quantidade total de blocos"   
+}
