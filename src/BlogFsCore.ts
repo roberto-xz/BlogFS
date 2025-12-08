ca097b9b1ed8ebe5d246a6078d3f508e56c0fa60
roberto-xz <devv-robert@outlook.com>
2025-12-08 10:00:00 -0300

    feat(core): implementar método open() e carregamento do meta buffer
    Implementações:
    - open(): recebe caminho do arquivo, verifica se é remoto e,
      caso seja local, lê o arquivo de metadados <path>_mt.fs.
    - Inicialização de meta_buffer e meta_view via DataView.
    - Tratamento simples de erro durante leitura.
    - Adicionada verificação de caminho remoto via isRemotePath().
    
    Observações:
    - Suporte a modo remoto ainda não implementado.

--- src/BlogFsCore.ts
+++ src/BlogFsCore.ts
@@ -1,1 +1,35 @@
 // [~] coded by roberto-xz
+import fs from "fs";
+
+export class BlogFsCore {
+    private meta_buffer!:ArrayBuffer;
+    private data_buffer!:ArrayBuffer;
+
+    private meta_view!:DataView;
+    private data_view!:DataView;
+    
+    private file_path:string = 'none';
+    private is_remote:Boolean = false;
+
+    public async open(file_path:string): Promise<void>{
+        this.is_remote = this.isRemotePath(file_path);
+        if ( this.is_remote == false ) { 
+             this.file_path = file_path;
+             try {
+                //lendo arquivo de metadados
+                const meta_file = fs.readFileSync(`${file_path}_mt.fs`);
+                this.meta_buffer = meta_file.buffer.slice(meta_file.byteOffset, meta_file.byteOffset + meta_file.byteLength);
+                this.meta_view = new DataView(this.meta_buffer);
+             }catch(Error) {throw Error;}
+        }else {
+            console.log('modo remoto ainda não implementado')
+        }
+    }
+
+    public isRemotePath(path: string): boolean {
+        try {
+            const url = new URL(path);
+            return url.protocol === "http:" || url.protocol === "https:";
+        } catch { return false;}
+    }
+}
